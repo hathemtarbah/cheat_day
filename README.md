18e0c0ca2f3caf5cd14f228888c6867ff746539d
Haytham <hathemtarbah@outlook.com>
2025-05-26 20:51:53 +0100

    cheat day 2

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # We do a little cheating
 
 i'm not ashamed to admit it
+cheat day count: 2
 
 ## "Features"
 
--- cheat0.py
+++ cheat0.py
@@ -2,3 +2,4 @@ from datetime import datetime
 
 print("cheat day")
 print(f"it is what it is innit {datetime.now()}")
+print("cheat day count: 2")
